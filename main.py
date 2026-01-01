09f12ceaebc3d94a7b25d89987c7b595dc929504
dataKernel <lancelot.saintleger@gmail.com>
2026-01-01 17:41:18 +0000

    creating grid

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("ceci est un test")
+def     create_grid()-> list:
+    grid = []
+    for i in range(3):
+        row = []
+        for j in range(3):
+            row.append("[ ]")
+        grid.append(row)
+    return grid
+
+test = create_grid()
+print(test)
