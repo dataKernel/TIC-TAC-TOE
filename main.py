60f8f466d2c8d219456b3500e47e3ec471f0756a
dataKernel <lancelot.saintleger@gmail.com>
2026-01-01 17:58:24 +0000

    (+):func printing grid

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ def     create_grid()-> list:
         grid.append(row)
     return grid
 
+def     print_grid(grid: list)-> None:
+    
+    for row in grid:
+        str = ""
+        for elem in row:
+            str += " " + elem
+        print(str)
+
 test = create_grid()
-print(test)
+print_grid(test)
